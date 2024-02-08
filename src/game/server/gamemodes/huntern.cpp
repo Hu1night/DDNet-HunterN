82886b634f32df546a4f8627bb688f383668362d
Hu1night <2506405864@qq.com>
2024-02-08 20:50:22 +0800

    回滚ConRevive

--- src/game/server/gamemodes/huntern.cpp
+++ src/game/server/gamemodes/huntern.cpp
@@ -69,7 +69,7 @@ static void ConRevive(IConsole::IResult *pResult, void *pUserData)
 	CPlayer *pPlayer = pSelf->GetPlayerIfInRoom((pResult->NumArguments() > 0) ? pResult->GetInteger(0) : pResult->m_ClientID);
 	if(!pPlayer) // If the player does not exist
 		pSelf->InstanceConsole()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "huntern", "invalid client id");
-	else if(!pResult->GetInteger(1) && pPlayer->GetCharacter() && pPlayer->GetCharacter()->IsAlive())
+	else if(pPlayer->GetCharacter() && pPlayer->GetCharacter()->IsAlive())
 		pSelf->InstanceConsole()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "huntern", "character is alive");
 	else
 	{	if(pPlayer->m_Class == CLASS_NONE)
@@ -96,7 +96,7 @@ CGameControllerHunterN::CGameControllerHunterN() :
 	InstanceConsole()->Register("htn_setclass", "i[class-id] ?i[CID] ?i[team-id] ?i[hunt-weapon]", CFGFLAG_CHAT | CFGFLAG_INSTANCE, ConSetClass, this, "给玩家设置职业（1平民,2猎人,4剑圣）");
 	InstanceConsole()->Register("htn_giveweapon", "i[weapon-id] i[slot] ?i[CID] ?i[ammo-num]", CFGFLAG_CHAT | CFGFLAG_INSTANCE, ConGiveWeapon, this, "给玩家武器");
 	InstanceConsole()->Register("htn_setheal", "i[health] ?i[armor] ?i[CID] ?i[max-health] ?i[max-armor]", CFGFLAG_CHAT | CFGFLAG_INSTANCE, ConSetHeal, this, "给玩家血量和盾");
-	InstanceConsole()->Register("htn_revive", "?i[CID] ?i[force-respawn]", CFGFLAG_CHAT | CFGFLAG_INSTANCE, ConRevive, this, "复活吧");
+	InstanceConsole()->Register("htn_revive", "?i[CID]", CFGFLAG_CHAT | CFGFLAG_INSTANCE, ConRevive, this, "复活吧");
 }
 
 void CGameControllerHunterN::OnResetClass(CCharacter *pChr) // 职业重置（出生后）
